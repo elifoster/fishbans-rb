652016694176ade8767ac6a3b4dea2dc631be747
elifoster <elifosterwy@gmail.com>
2017-02-17 22:09:31 -0800

    Update examples

--- examples/standard.rb
+++ examples/standard.rb
@@ -1,17 +1,16 @@
-# Creating a string based on the get_total_bans method.
-
 require 'fishbans'
 
-username = 'satanicsanta'
-bans = Fishbans.get_total_bans(username)
-if bans.is_a?(Fixnum)
+print 'Enter a username: '
+username = gets.chomp
+begin
+  bans = Fishbans.get_total_bans(username)
   if bans > 0
-    message = "#{username} has been banned! What a loser! They've " \
-              "been banned #{bans} times!"
+    message = "#{username} has been banned! What a loser! They've been banned #{bans} times!"
   else
     message = "#{username} has not been banned! What a gentle person!"
   end
-else
-  message = "Error: #{bans}"
+rescue RuntimeError => e
+  message = "Error: #{e.message}"
+ensure
+  puts message
 end
-puts message
